233e3bb19ef464eeb2e713ca5abd6c18dae9fefa
maheshtawade2308 <maheshtawade2308@gmail.com>
2025-10-21 14:38:25 +0530

    kamgar card  completed

--- src/utils/QRCodeGeneratorKid.js
+++ src/utils/QRCodeGeneratorKid.js
@@ -10,7 +10,11 @@ const QRCodeGenerator = ({ mobile, regId }) => {
     const qrData = `${registrationNumber},${mobileNo}`;
 
     if (canvasRef.current) {
-      QRCode.toCanvas(canvasRef.current, qrData, { width: 90, margin: 0 }, (error) => {
+      QRCode.toCanvas(canvasRef.current, qrData, { width: 90, margin: 0 ,  color: {
+            dark: '#000000',       // QR code dots
+            light: '#0000'   // Background
+          }
+}, (error) => {
         if (error) console.error('QR Code generation error:', error);
       });
     }
